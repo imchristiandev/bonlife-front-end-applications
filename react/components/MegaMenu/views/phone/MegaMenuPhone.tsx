e5e90a85e7a47ac4abd076563dbaa1744a607fc4
christiandev <imchristiandev@gmail.com>
2024-02-07 23:27:49 -0500

    feat: development of navigation logic

--- react/components/MegaMenu/views/phone/MegaMenuPhone.tsx
+++ react/components/MegaMenu/views/phone/MegaMenuPhone.tsx
@@ -3,6 +3,7 @@ import MenuContext from '../../../../context/menuContext'
 
 // @ts-ignore
 import { Icon } from 'vtex.store-icons'
+import { useRuntime } from 'vtex.render-runtime'
 
 import { IMenuItem } from '../../../../typings/MegaMenu'
 import { useQuery } from 'react-apollo'
@@ -12,6 +13,7 @@ import { updateCurrentMenu } from '../../../../utils/updateCurrentMenu'
 export const MegaMenuPhone = () => {
   const { currentMenu, mobileMenuItemsBehavior, setCurrentMenu } = React.useContext(MenuContext)
   const [menuId, setMenuId] = useState('')
+  const { navigate } = useRuntime()
 
   const { data } = useQuery(GET_MENU, { variables: { menuId } });
 
@@ -26,27 +28,40 @@ export const MegaMenuPhone = () => {
     setMenuId( menuId );
   }
 
+  const handleNavigation = (slug: string) => {
+    const newSlug = slug.startsWith('/') ? slug : `/${slug}`
+    navigate({
+      to: `${newSlug}/`,
+    })
+  }
+
   return (
     currentMenu ?
-      <ul>
+      <ul className='w-100 pa6'>
         {
           currentMenu.map((menu: IMenuItem) => (
             <li key={menu.id}>
               {
                 mobileMenuItemsBehavior === 'openParentLink' ?
-                <button onClick={() => updateChildrenMenu( menu.id )}>
-                  <Icon id={ menu.icon } /> { menu.name }
-                </button> :
-                <div>
-                  <Icon id={ menu.icon } /> { menu.name }
-                </div>
-              }
-              {
-                menu.hasChildren ?
-                <div>
-                  <Icon id='nav-caret--right' />
+                <div className='flex justify-between'>
+                  <button onClick = { () => {handleNavigation( menu.slug )} } >
+                    <Icon id={ menu.icon } /> { menu.name }
+                  </button>
+                  {
+                  menu.hasChildren &&
+                  <button onClick={() => updateChildrenMenu( menu.id )}>
+                    <Icon id={ menu.icon } />
+                  </button>
+                  }
                 </div> :
-                null
+                <button onClick = { () => {handleNavigation( menu.slug )} } >
+                  <div>
+                    <Icon id={ menu.icon } /> { menu.name }
+                  </div>
+                  <div>
+                    <Icon id={ menu.icon } />
+                  </div>
+                </button>
               }
             </li>
           ))
